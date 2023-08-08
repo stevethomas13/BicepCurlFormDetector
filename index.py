5cd25274791c5b60effb23c25aa1415dc3e82f37
Steve Thomas <steve13thomas@gmail.com>
2023-08-08 18:10:12 +0100

    added divider

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import streamlit as st
 def main():
     st.title("Bicep Curl Form Detector")
     st.subheader("This app allows you to judge the form of your bicep curls using OpenCV and Mediapipe")
+    st.divider()
     st.text("Before we begin, let's take you through the onboarding process.")
     hand = ""
     if st.button("Click to start."):
