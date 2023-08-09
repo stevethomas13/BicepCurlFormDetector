a1adc078429e81fb5b64cb91a40687ab75ba21e1
Steve Thomas <steve13thomas@gmail.com>
2023-08-09 20:32:41 +0100

    added drop down menu

--- index.py
+++ index.py
@@ -6,14 +6,17 @@ def main():
     st.divider()
     st.text("Before we begin, let's take you through the onboarding process.")
     hand = ""
+    # start_button_not_pressed = false
+    # if start_button_not_pressed: 
+    #     st.button("Click to start.",  )
+    #     hand = st.selectbox(
+    #     "Which hand would you like to use for the demo?",
+    #     ("Left", "Right")
     if st.button("Click to start." ):
-        hand = st.selectbox(
-        "Which hand would you like to use for the demo?",
-        ("Left", "Right")
-    )
+        arm_selected = st.selectbox( "Which arm would you like to use?" , ["Left", "Right"] )
+        hand = arm_selected
 
     st.text(hand)
-
 
 
 if __name__ == '__main__':
