47b37ec33f3d517a7f635f7adf3cc820b6953b31
Steve Thomas <steve13thomas@gmail.com>
2023-08-09 09:10:25 +0100

    minor changes

--- index.py
+++ index.py
@@ -6,19 +6,14 @@ def main():
     st.divider()
     st.text("Before we begin, let's take you through the onboarding process.")
     hand = ""
-    if st.button("Click to start."):
-        st.text("Which hand would you like to use for the demo?")
-        if st.checkbox("Left"):
-            hand = "Left"
-        if st.checkbox("Right"):
-            hand = "Right"
+    if st.button("Click to start." ):
+        hand = st.selectbox(
+        "Which hand would you like to use for the demo?",
+        ("Left", "Right")
+    )
 
+    st.text(hand)
 
-    slider_value = st.slider("Slider", min_value=0.5, max_value=3.5)
-    st.text(f"Slider value is {slider_value}")
-
-    st.sidebar.text("text on side panel")
-    st.sidebar.checkbox("Side Panel Checkbox")
 
 
 if __name__ == '__main__':
