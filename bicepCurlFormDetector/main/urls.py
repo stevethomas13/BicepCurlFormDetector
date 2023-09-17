0cd08ed4f95a7165cdb70e16215994e313432e1f
Steve Thomas <steve13thomas@gmail.com>
2023-09-17 21:09:13 +0100

    created index view page

--- bicepCurlFormDetector/main/urls.py
+++ bicepCurlFormDetector/main/urls.py
@@ -3,13 +3,8 @@ from django.urls import path
 from . import views
 
 
-app_name = "polls"
+app_name = "main"
 urlpatterns = [
-    path( "", views.IndexView.as_view(), name='index' ),
+    path( "", views.index, name='index' ),
     # ex: /polls/5/
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    # ex: /polls/5/results/
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # ex: /poplls/5/vote/
-    path("<int:pk>/vote/", views.vote, name="vote"),
 ]
