6c146271e7dd592663cfc81cd076a9f5c331511a
Steve Thomas <steve13thomas@gmail.com>
2023-09-21 18:22:26 +0100

    added static files path

--- bicepCurlFormDetector/main/urls.py
+++ bicepCurlFormDetector/main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 
 app_name = "main"
+
 urlpatterns = [
     path( "", views.index, name='index' ),
     # ex: /polls/5/
 ]
+
+urlpatterns += staticfiles_urlpatterns()
