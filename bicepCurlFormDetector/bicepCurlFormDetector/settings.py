9d961d9135ca7896721cd06c0c9fb4df08131571
Steve Thomas <steve13thomas@gmail.com>
2023-09-23 18:59:43 +0100

    defined where to search for templates

--- bicepCurlFormDetector/bicepCurlFormDetector/settings.py
+++ bicepCurlFormDetector/bicepCurlFormDetector/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bicepCurlFormDetector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'main/templates/main'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
