e29129bf9c5217f649433a21459ec70cd22ab24d
Steve Thomas <steve13thomas@gmail.com>
2023-09-28 13:44:17 +0100

    create database and made migrations

--- bicepCurlFormDetector/main/models.py
+++ bicepCurlFormDetector/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    HAND_OPTIONS = [
+        ("Lt", "Left"),
+        ("Rt", "Right"),
+        ("Null", "Select"),
+    ]
+    hand = models.CharField(max_length=7, choices=HAND_OPTIONS)
